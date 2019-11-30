c496bd5275715f898e20ca0bb90849b75c5a998b
yeari <yeari0@naver.com>
2019-11-30 21:40:29 +0900

    Material Calendar add

--- MoebiusPlanner/app/src/main/java/com/example/moebiusplanner/MainActivity.java
+++ MoebiusPlanner/app/src/main/java/com/example/moebiusplanner/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.moebiusplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public MaterialCalendarView mCalendar;
+    public TextView mTextDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCalendar = (MaterialCalendarView) findViewById(R.id.calendarView);
+        mTextDate = (TextView) findViewById(R.id.textDate);
+
+        //mCalendar.addDecorators(new SaturdayDecorator(), new SundayDecorator());
     }
 }
